46dabc2b02d11ca4000918500b9c92215aae1b61
Qs <quanangel@outlook.com>
2019-07-01 18:22:30 +0800

    add--api and token logic

---
+++ src/Api.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Qs\api;
+
+use think\facade\Request;
+use Qs\log\Log;
+
+class Api{
+
+    // 允许的method
+    protected $allowMethod = ' GET, POST, PUT, DELETE, OPTIONS';
+    // 请求信息实例
+    protected $request;
+    // 请求类型: GET/POST/PUT/DELETE/OPTIONS
+    protected $method;
+    // 验证TOKEN
+    protected $authToken;
+    // 根据TOKEN获取user_id
+    protected $userId;
+    // 验证器
+    protected $validate;
+    // token插件
+    protected $token;
+    // 日记
+    protected $log;
+
+    // 响应信息
+    protected $returnData;
+    // 请求参数数组
+    protected $paramData;
+    
+
+    // 初始化
+    public function __construct() {
+        // 默认返回信息
+        $this->returnData = ['status'=>0, 'msg'=>lang('not authorized')];
+        // 允许所有跨域
+        header("Access-Control-Allow-Origin: *");
+        // 允许跨域传COOKICE
+        header('Access-Control-Allow-Credentials: true');
+        // 允许接收的头
+        header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Auth-Token");
+        // 允许的请求类型
+        header('Access-Control-Allow-Methods:' . $this->allowMethod);
+        // 类型、编码
+        header('Content-Type:application/json; charset=utf-8');
+        // 获取请求信息
+        $this->request = Request::instance();
+        // 获取验证TOKEN
+        $this->authToken = $this->request->header('Auth-Token');
+        if ( $this->authToken && $this->authToken != 'undefined' ) {
+            $this->userId = base64_decode($this->authToken);
+            $this->userId = explode('_', $this->userId);
+            $this->userId = isset($this->userId[1]) && is_numeric($this->userId[1]) ? $this->userId[1] : null;
+        }
+
+        $this->method = strtolower($this->request->method());
+        if ( !$this->auth_method() ) {
+            http_response_code(406);
+            exit(json_encode($this->returnData));
+        }
+        $this->token = new Token;
+        $this->log = Log::instance();
+        $this->paramData = $this->request->param();
+    }
+
+    /**
+     * @Author: Qs
+     * @Name: 验证请求头
+     * @Note: 
+     * @Time: 2018/6/25 16:16 
+     * @param   string  $method   请求头类型：GET、POST、PUT、DELETE、OPTIONS
+     * @return  boolean           验证结果：true or false
+     **/
+    protected function auth_method( $method = '' ) {
+        $method = $method ? : $this->method;
+        if ( stripos( $this->allowMethod, $method ) ) return true;
+        return false;
+    }
+    
+    /**
+     * @Author: Qs
+     * @Name: 验证qs_token
+     * @Note: 
+     * @Time: 2018/6/25 16:36 
+     * @return  array   返回数组包含：status、msg、username、qs_token
+     **/
+    protected function auth_token() {
+        return $this->token->auth_token($this->authToken);
+    }
+
+    protected function request() {
+        
+    }
+
+}
---
+++ src/Token.php
@@ -0,0 +1,124 @@
+<?php 
+
+namespace Qs\api;
+
+use Qs\redis\QsRedis;
+use Qs\log\Log;
+
+class Token {
+
+    // 用户信息表
+    private $userTable = 'now_users';
+    // 日记实例
+    protected $log;
+    // redis
+    protected $redis;
+
+    public function __construct(){
+        // TODO: 初始化
+        $this->log = Log::instance();
+        $this->redis = new QsRedis(config('redis.'));
+    }
+
+
+    /**
+     * @Author : Qs
+     * @Name   : 生成token
+     * @Note   : 
+     * @Time   : 2018/9/18 0:55
+     * @param   integer     $userId     用户ID
+     * @param   string      $oldToken   用户表上的token
+     * @param   integer     $time       生成token附加值
+     * @return  string                  新的token
+     **/
+    private function create_token( $userId, $time = 3600 ){
+        $token = md5($userId . rand(5, 32) . (time() + $time) );
+        if ( $this->is_repeat($token) ) $token = $this->create_token($userId, $time + 1);
+        return $token;
+    }
+
+    /**
+     * @Author : Qs
+     * @Name   : 生成scan_token
+     * @Note   : 
+     * @Time   : 2018/9/13 0:58
+     * @param   integer     $userId     用户ID
+     * @param   string      $token      用户表上的token
+     * @param   integer     $time       生成token附加值
+     * @return  string                  新的scan_token
+     **/
+    private function create_scan ( $userId, $time = 604800 ) {
+        $scan = md5( $userId . rand(5, 32) . (time() + $time) );
+        if ( $this->is_repeat($scan, 'scan_token') ) $data = $this->create_token($userId, $time + 1);
+        return $scan;
+    }
+
+    /**
+     * @Author : Qs
+     * @Name   : 生成token/scan_token
+     * @Note   : 
+     * @Time   : 2018/9/18 15:35
+     * @param    integer     $userId    用户ID
+     * @param    string      $type      生成类型
+     * @return   string                 生成对应的token
+     **/
+    public function token ( $userId, $type = 'token' ) {
+        $newToken = false;
+        try {
+            switch ( $type ) {
+                case 'token':
+                    $newToken = $this->create_token($userId);
+                    break;
+                case 'scan':
+                    $newToken = $this->create_scan($userId);
+                    break;
+            }
+            // 删除已有TOKEN
+            $this->redis->rm($userId);
+            // 添加新的TOKEN
+            $this->redis->set($userId, $newToken, 0);
+            $newToken = base64_encode($newToken . '_' . $userId . '_' . $type);
+        } catch ( \Exception $e ) {
+            $newToken = false;
+            $this->log->save($e->getMessage(), 'token_error');
+        }
+        return $newToken;
+    }
+
+    /**
+     * @Author : Qs
+     * @Name   : 判断某个字段值是否重复
+     * @Note   : 
+     * @Time   : 2018/9/13 1:19
+     * @param   string      $string     字段值
+     * @param   string      $key        字段
+     * @return  boolean
+     **/
+    private function is_repeat( $string, $key = 'token' ){
+        $tmp = $this->redis->has($string);
+        if ( $tmp ) return true;
+        return false;
+    }
+
+    /**
+     * @Author : Qs
+     * @Name   : 验证token/scan_token
+     * @Note   : 
+     * @Time   : 2018/9/18 16:13
+     * @param    string    $authToken    需验证的TOKEN信息
+     * @return   boolean
+     **/
+    public function auth_token ( $authToken ) {
+        $authToken = base64_decode($authToken);
+        $authToken = explode('_',$authToken);
+        if ( count($authToken) != 3 ) return false;
+        $token = $authToken[0];
+        $userId = $authToken[1];
+        $type = $authToken[2] == 'scan' ? 'scan_token' : $authToken[2];
+        $userTmp = $this->redis->get($userId);
+        if ($userTmp == $token) return true;
+        return false;
+    }
+   
+
+}
